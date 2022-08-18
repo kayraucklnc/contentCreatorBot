7540a5f30b01a47ede28e29e6ebab0baaea07f7d
Kayra <kayraucklnc@gmail.com>
2022-08-19 00:47:21 +0300

    Better formatting - Bugfix
    + buttons working
    + formatting better

--- scripts/cogs/controller.py
+++ scripts/cogs/controller.py
@@ -1,5 +1,4 @@
-from ast import Delete
-from hmac import digest
+import asyncio
 from instagramBot import InstaBot
 import traceback
 import io
@@ -11,10 +10,10 @@ from reddit import redditScrapper
 class Settings:
     def __init__(self):
         self.commentAmount = 2
-        self.printerDelta = 60*15
+        self.printerDelta = 60*0.2
         self.digestDelta = 60*100
         self.filterTime = "hour"
-        self.subs = ["askreddit", "showerthoughts", "askmen", "explainlikeimfive", "tooafraidtoask", "jokes", "unethicallifeprotips", "crazyideas"]
+        self.subs = ["askreddit", "unethicallifeprotips", "askmen", "explainlikeimfive", "tooafraidtoask", "jokes", "showerthoughts", "crazyideas"]
         self.mode = len(self.subs)-1
         
     def getSub(self):
@@ -30,26 +29,20 @@ instaBot = InstaBot()
 class Controller(commands.Cog):   
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.index = 0
+        self.general = 1008439697678287010
+        self.upcoming = 1008464497574428803
+        self.posted = 1008812515339276430
+        self.deleted = 1008812540744192120
+        
         self.reddit = redditScrapper(settings.getSub())
         self.printer.start()
-        self.digester.start()
-        self.bot.loop.create_task(self.setChannelIds())
-        
-        
-    async def setChannelIds(self):
-        if not self.bot.is_ready():
-            await self.bot.wait_until_ready()
-
-        self.general = self.bot.get_channel(1008439697678287010)
-        self.upcoming = self.bot.get_channel(1008464497574428803)
-        self.posted = self.bot.get_channel(1008812515339276430)
-        self.deleted = self.bot.get_channel(1008812540744192120)
+        # self.digester.start()
 
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("Bot is online")
+
         
     @commands.command(name="ping")
     async def ping(self, ctx):
@@ -58,34 +51,37 @@ class Controller(commands.Cog):
     @commands.command()
     async def purge(self, ctx):
         await ctx.channel.purge()
-
-
+        
+    def printerWrapper(self): 
+        postArr = self.reddit.getRedditPostAsImage(postCount=1, commentCount=settings.commentAmount, filter=settings.filterTime, isTesting=False)
+        self.reddit.changeSub(settings.getSub())
+        
+        print("Sending Images now!")
+        listOfImgs = []
+        for post in postArr:
+            with io.BytesIO() as image_binary:
+                        post.img.save(image_binary, 'PNG')
+                        image_binary.seek(0)
+                        listOfImgs.append(discord.File(fp=image_binary, filename=f'{post.title}.png'))
+        
+        return postArr, listOfImgs
+    
     @tasks.loop(seconds=settings.printerDelta)
     async def printer(self):
-        view = UpcomingView()
         try:
+            view = UpcomingView(self)
             print("------- Creating Images -------")
-            postArr = self.reddit.getRedditPostAsImage(postCount=1, commentCount=settings.commentAmount, filter=settings.filterTime, isTesting=False)
-            self.reddit.changeSub(settings.getSub())
             
-            print("Sending Images now!")
-            listOfImgs = []
-            for post in postArr:
-                with io.BytesIO() as image_binary:
-                            post.img.save(image_binary, 'PNG')
-                            image_binary.seek(0)
-                            listOfImgs.append(discord.File(fp=image_binary, filename=f'{post.title}.png'))
+            postArr, listOfImgs = await asyncio.to_thread(self.printerWrapper)
+            view.add_item(discord.ui.Button(label="Post",style=discord.ButtonStyle.link,url=postArr[0].url))            
                 
-            await self.upcoming.send(postArr[0].title,files=listOfImgs, view=view)
+            await self.bot.get_channel(self.upcoming).send(postArr[0].title,files=listOfImgs, view=view)
             print("Images sent")
             
-            
         except Exception as e:
-            await self.general.send(f"Failed 😥 \n {e}")
+            await self.bot.get_channel(self.general).send(f"Failed 😥  ```{e}```")
             print(traceback.format_exc())
             
-            
-    @tasks.loop(seconds=settings.digestDelta)
     async def digester(self):
         # SHOULD DISABLE BUTTONS BEFORE DIGESTING
         try:
@@ -98,49 +94,50 @@ class Controller(commands.Cog):
                 url = instaBot.uploadAlbum(toDigest.content, "Caption")
                 view.add_item(discord.ui.Button(label="Post",style=discord.ButtonStyle.link,url=url))
 
-                await self.bot.get_channel(1008812515339276430).send(toDigest.content, files=[await f.to_file() for f in toDigest.attachments], view=view)
+                await self.bot.get_channel(self.posted).send(toDigest.content, files=[await f.to_file() for f in toDigest.attachments], view=view)
                 await toDigest.delete()
             else:
-                await self.general.send(f"Nothing do digest 😁 - {time.ctime()}")
+                await self.bot.get_channel(self.general).send(f"Nothing do digest 😁 - {time.ctime()}")
                 
               
         except Exception as e:
-            await self.general.send(f"Digest failed - {time.ctime()}😥 \n {e}")
+            await self.bot.get_channel(self.general).send(f"Digest failed - {time.ctime()}😥 \n {e}")
             print(traceback.format_exc())
-
- 
-
+        
     @printer.before_loop
     async def before_printer(self):
         await self.bot.wait_until_ready()
-        
-    @digester.before_loop
-    async def before_printer(self):
-        await self.bot.wait_until_ready()
-        
 
 class UpcomingView(discord.ui.View):
+    def __init__(self, other):
+        self.other = other
+        self.postedView = PostedView()
+        super().__init__()
+    
     @discord.ui.button(label='Accept', style=discord.ButtonStyle.success)
-    async def accept(self, interaction: discord.Interaction, button: discord.ui.Button):
-        
-        
-        await self.bot.get_channel(1008812515339276430).send(interaction.message.content, files=[await f.to_file() for f in interaction.message.attachments])
+    async def accept(self, interaction: discord.Interaction, button: discord.ui.Button):        
+        try:
+            for child in self.children:
+                child.disabled=True
+            await interaction.response.edit_message(view=self)
+                
+            files = [await f.to_file() for f in interaction.message.attachments]
+            content = interaction.message.content
+            await interaction.message.delete()
+            await self.other.bot.get_channel(1008812515339276430).send(content, files=files, view = self.postedView)
+        except:
+            print(traceback.format_exc())
 
         
-        await interaction.response.defer()
-        await interaction.message.delete()
         instaBot.uploadAlbum(interaction.message.content, "caption")
 
     @discord.ui.button(label='Decline', style=discord.ButtonStyle.danger)
     async def decline(self, interaction: discord.Interaction, button: discord.ui.Button):
-        # button = manage_components.create_button(style=ButtonStyle.URL, label="Your channel", url=f'https://discord.com/channels/{member.guild.id}/{channel.id}')
+        for child in self.children:
+            child.disabled=True
+        await interaction.response.edit_message(view=self)
         
         await interaction.message.delete()
-        await interaction.response.defer()
-        
-        # for child in self.children:
-        #     child.disabled=True
-        # await interaction.response.edit_message(view=self)
         
 class PostedView(discord.ui.View):
     @discord.ui.button(label='Delete Post', style=discord.ButtonStyle.danger)
--- scripts/reddit.py
+++ scripts/reddit.py
@@ -11,33 +11,38 @@ from util import human_format
 from better_profanity import profanity
 import time
 import lorem
+import re
+
 
 class TestSubmission:
     class author:
         def __init__(self):
             self.name = "TestAuthor"
     
-    def __init__(self):
-        time.sleep(2)
-        self.title = lorem.paragraph()
-        self.selftext = lorem.paragraph() + lorem.paragraph() + lorem.paragraph()
-        # self.title = lorem.sentence()
-        # self.selftext = ""
+    def __init__(self):      
+        # self.title = lorem.paragraph()
+        # self.selftext = lorem.paragraph()
+        
+        self.title = 10*"a"
+        self.selftext = 1650*"a"
+        
         self.num_comments = 3600
         self.score = 25000
         self.author = TestSubmission.author()
         self.comments = list()
-        self.icon_img = "https://www.anatolihome.com/image/cache/catalog/products/8680571800562/1-250x250.jpg"
+        self.icon_img = "./assets/reddit.png"
+        self.url = "https://www.reddit.com/r/AskReddit/"
 
 
 
 class PostSubmission:
-    def __init__(self, title, author, img, icon_img, isPost = True):
+    def __init__(self, title, author, img, icon_img, url, isPost = True):
         self.title = title
         self.author = author
         self.img = img
         self.icon_img = icon_img
         self.isPost = isPost
+        self.url = url
     
 
 
@@ -45,16 +50,19 @@ class redditScrapper:
     def __init__(self, sub, bgres = 1000):
         print("Initialized")
         self.sub = sub
-        self.textWrapLen = 62
+        self.textWrapLen = 58
+        self.pageLength = 62
         self.imageSize = 1000
         self.textWidth = 1000
         self.textHeight = 38
         self.elementPadding = 10
         self.textLeftPadding = 90
         self.resetBG(bgres)
+        self.urlPattern = re.compile('\[(.*?)\]\(.*?\)')
+
 
         self.font = ImageFont.truetype("LucidaSansDemiBold.ttf", 25)
-        self.mediumFont = ImageFont.truetype("calibrib.ttf", 24)
+        self.mediumFont = ImageFont.truetype("calibrib.ttf", 26)
         self.smallFont = ImageFont.truetype("calibrib.ttf", 21)
         
         self.reddit = praw.Reddit(
@@ -64,37 +72,32 @@ class redditScrapper:
             check_for_async=False
             )
         
-
     def changeSub(self, sub):
         self.sub = sub
-        
     
     def resetBG(self, res):
-        response = requests.get('https://picsum.photos/' + str(res))
+        # response = requests.get('https://picsum.photos/' + str(res))
         # response = requests.get(f"https://picsum.photos/{res}/{res}")
-        self.background = Image.open(BytesIO(response.content))
-        # self.background = Image.open("./outputs/pic.jpg")
+        # self.background = Image.open(BytesIO(response.content))
+        self.background = Image.open("./outputs/pic.jpg")
         enhancer = ImageEnhance.Brightness(self.background)
         self.background = enhancer.enhance(0.6)
-        
         
     def setHeight(self, text, content=None):
         content = content if content else ""
         lineCount = len(textwrap.wrap(text + content, width=self.textWrapLen))
         return max(lineCount + 4, 2)*29 + self.elementPadding
 
-    def addText(self, text, image, textColor = "#ffffff", content = None):
-        text = profanity.censor(text)
+    def addTitleContent(self, text, image, textColor = "#ffffff", content = None, isHeadEmpty = True):
         draw = ImageDraw.Draw(image)
         
         margin = self.textLeftPadding
-        offset = self.imageSize/2 - self.textHeight/2 + self.elementPadding + 29
+        offset = self.imageSize/2 - self.textHeight/2 + self.elementPadding + (29 if isHeadEmpty else 0)
         for line in textwrap.wrap(text, width=self.textWrapLen):
             draw.text((margin, offset), line, textColor, font=self.font)
             offset += self.font.getbbox(line)[3]
         
         if(content):
-            content = profanity.censor(content)
             for line in textwrap.wrap(content, width=self.textWrapLen):
                 draw.text((margin, offset+29), line, "#CBCBCC", font=self.font)
                 offset += self.font.getbbox(line)[3]
@@ -144,10 +147,14 @@ class redditScrapper:
         mask = ImageChops.darker(mask, im.split()[-1])
         im.putalpha(mask)
         
-    def addIcon(self, iconPath, image, posY):
-        response = requests.get(iconPath)
-        
-        logo = Image.open(BytesIO(response.content))
+    def addIcon(self, iconPath, image, posY, isIconOnUrl):
+        logo = None
+        if(isIconOnUrl):
+            response = requests.get(iconPath)
+            logo = Image.open(BytesIO(response.content))
+        else:
+            logo = Image.open(iconPath)
+            
         logo = logo.resize((60,60))
         
         logo = logo.convert('RGBA')
@@ -157,44 +164,52 @@ class redditScrapper:
         
         return logo
 
-    def createImageWithText(self, title, icon = ""):
+    def createImageWithText(self, title, icon = "", isIconOnUrl = True):
         bg = self.background.copy()
         self.textHeight = self.setHeight(title)
         self.blurBox(bg)
-        self.addText(title, bg)
+        self.addTitleContent(title, bg)
         
         if(icon != ""):
-            self.addIcon(icon, bg, int(self.imageSize/2-self.textHeight/2+self.elementPadding))
+            self.addIcon(icon, bg, int(self.imageSize/2-self.textHeight/2+self.elementPadding), isIconOnUrl=isIconOnUrl)
             
         bg.show()
         return bg
-        
-        
+            
     def isCommentBadCheck(self, comment):
         isRemoved = (comment.body == "[removed]")
-        isBot = re.search('moderator', comment.author.name, re.IGNORECASE)
+        isBot = False if not comment.author else re.search('moderator', comment.author.name, re.IGNORECASE)
+        isSticky = comment.stickied
         
-        return isRemoved or isBot
+        return isRemoved or isBot or isSticky
     
     def getRedditPostAsImage(self, filter = "day", postCount = 1, commentCount = 4, saveOnCreate = False, isTesting = False):
         listOfPosts = []
         print("Gather Post Started")
         subredditHelper = self.reddit.subreddit(self.sub)
-        
-        submissionArray = [TestSubmission() for i in range(postCount)] if isTesting else subredditHelper.top(time_filter=filter, limit=postCount)
-        subIconPath = TestSubmission().icon_img if (isTesting or (len(subredditHelper.icon_img) < 8 )) else subredditHelper.icon_img
+        submissionArray = [TestSubmission() for i in range(postCount)] if isTesting else subredditHelper.top(time_filter=filter, limit=postCount)        
+        subIconPath = TestSubmission().icon_img if ((not subredditHelper.icon_img) or isTesting or (len(subredditHelper.icon_img) == 0)) else subredditHelper.icon_img
+        isIconOnUrl = (not ((not subredditHelper.icon_img) or isTesting or (len(subredditHelper.icon_img) == 0)))
             
         for submission in submissionArray:
             self.resetBG(self.imageSize)
             title = submission.title
             content = submission.selftext
             name = submission.author.name if submission.author != None else "Unknown"
+            
+            title = profanity.censor(title)
+            title = self.urlPattern.sub(r'\1', title)
+            
+            content = profanity.censor(content)
+            content = self.urlPattern.sub(r'\1', content)
+                     
+            
             bg = self.background.copy()
             self.textHeight = self.setHeight(title, content)
             self.blurBox(bg)
-            self.addText(title, bg, content=content)
-            self.addIcon(subIconPath, bg, int(self.imageSize/2-self.textHeight/2+self.elementPadding))
-            self.addMediumCustomText(bg, "r/" + self.sub, (self.elementPadding*3 + 60, int(self.imageSize/2-self.textHeight/2+self.elementPadding)), "#ffffff", textAnchor="lt", storoke_fill=(255, 69, 0, 10), stroke_width=1)
+            self.addTitleContent(title, bg, content=content)
+            self.addIcon(subIconPath, bg, int(self.imageSize/2-self.textHeight/2+self.elementPadding), isIconOnUrl)
+            self.addMediumCustomText(bg, "r/" + self.sub, (self.elementPadding*3 + 60, int(self.imageSize/2-self.textHeight/2+self.elementPadding)), "#FF4500", textAnchor="lt", storoke_fill="#000000", stroke_width=1)
             self.addCustomText(bg, human_format(submission.score), (self.elementPadding + 30, int(self.imageSize/2-self.textHeight/2+self.elementPadding + 85)), "#fa6505")
             self.addCustomText(bg, "/u/" + name, (self.imageSize - self.elementPadding, int(self.imageSize/2+self.textHeight/2-self.elementPadding)), "#ffffff", textAnchor = "rs")
             self.addCustomText(bg, human_format(submission.num_comments), (self.elementPadding + 30, int(self.imageSize/2-self.textHeight/2+self.elementPadding + 110)), "#39ceff")
@@ -203,26 +218,28 @@ class redditScrapper:
             if(saveOnCreate):
                 bg.save("outputs/" + str(int(round(time.time() * 1000))) + " - " +  submission.author.name + ".jpg")
             else:
-                listOfPosts.append(PostSubmission(title, name, bg, subIconPath))
+                listOfPosts.append(PostSubmission(title, name, bg, subIconPath, submission.url))
             
             for count, comment in enumerate(submission.comments):
                 if(commentCount == count):
                     break
                                 
-                if(not self.isCommentBadCheck(comment)):
+                if(self.isCommentBadCheck(comment)):
                     continue
                 
                 commentBody = comment.body
+                commentBody = profanity.censor(commentBody)
+                commentBody = self.urlPattern.sub(r'\1', commentBody)
                 
                 commentAuthorName = comment.author.name if comment.author else "[deleted]"
                 commentImage = self.background.copy()                
                 
                 self.textHeight = self.setHeight(commentBody)
                 self.blurBox(commentImage)
-                self.addText(commentBody, commentImage, "#c9c9c9")
+                self.addTitleContent(commentBody, commentImage, "#e8e8e8", isHeadEmpty=False)
                 self.addCustomText(commentImage, human_format(comment.score), (self.elementPadding + 30, int(self.imageSize/2-self.textHeight/2+self.elementPadding + 85)), "#fa6505")
                 self.addCustomText(commentImage, "/u/" + commentAuthorName, (self.imageSize - self.elementPadding, int(self.imageSize/2+self.textHeight/2-self.elementPadding)), "#ffffff", textAnchor = "rs")
-                self.addIcon(subIconPath, commentImage, int(self.imageSize/2-self.textHeight/2+self.elementPadding))
+                self.addIcon(subIconPath, commentImage, int(self.imageSize/2-self.textHeight/2+self.elementPadding), isIconOnUrl)
                    
                 if(saveOnCreate):
                     commentImage.save("outputs/" + str(int(round(time.time() * 1000))) + " - " +  submission.author.name + ".jpg")
