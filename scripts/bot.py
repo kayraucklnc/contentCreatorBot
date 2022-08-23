5885173e90e9a1a26fd0c136e8aeb475bedc8681
Kayra <kayraucklnc@gmail.com>
2022-08-23 17:16:29 +0300

    final

--- scripts/bot.py
+++ scripts/bot.py
@@ -4,7 +4,7 @@ import os
 from reddit import redditScrapper
 
 app = redditScrapper("askreddit")
-app.getRedditPostAsImage(postCount=1, commentCount=0, filter="week", isTesting=True, saveOnCreate=True)
+# app.getRedditPostAsImage(postCount=1, commentCount=0, filter="week", isTesting=True, saveOnCreate=True)
 # app.getPostFromLink("https://www.reddit.com/r/askscience/comments/wtjdsq/what_would_happen_if_you_poured_a_sunsized_bucket/", saveOnCreate=True)
 
 
